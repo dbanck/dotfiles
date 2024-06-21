3245f39bbbfa4f6377ee1ae9f2e966469a67f02a
Daniel Banck <daniel@dbanck.de>
2024-06-21 15:56:28 +0200

    Update fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,11 +5,14 @@ set -x LANG en_US.UTF-8
 
 set -x GOPATH ~/go
 
-set -x PATH /usr/local/sbin ~/.rbenv/shims $PATH ~/Applications/bin ~/.npm/bin ~/.cargo/bin
-set -x PATH $GOPATH/bin $PATH 
+set -x PATH $GOPATH/bin $PATH ~/Applications/bin ~/.npm/bin ~/.cargo/bin
 
 set -x EDITOR /usr/local/bin/vim
 
-starship init fish | source
-op completion fish | source
-source ~/.config/op/plugins.sh
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    /opt/homebrew/bin/brew shellenv | source
+    starship init fish | source
+    op completion fish | source
+    source ~/.config/op/plugins.sh
+end
