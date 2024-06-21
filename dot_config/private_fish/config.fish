a1b9ca3a20d6f00ba5625479623be13fa45a28f8
Daniel Banck <daniel@dbanck.de>
2024-06-21 20:34:07 +0200

    Fix editor and shim path

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -6,8 +6,10 @@ set -x LANG en_US.UTF-8
 set -x GOPATH ~/go
 
 set -x PATH $GOPATH/bin $PATH ~/Applications/bin ~/.npm/bin ~/.cargo/bin
+# Add the shims to the end of PATH as fallback for VS Code
+set -x PATH $PATH ~/.local/share/mise/shims
 
-set -x EDITOR /usr/local/bin/vim
+set -x EDITOR /opt/homebrew/bin/vim
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
