403af2c762f217061f4037d6666f0040ec7d80d8
Daniel Banck <daniel@dbanck.de>
2024-06-25 18:29:53 +0200

    Add neovim config

---
+++ dot_config/nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,23 @@
+-- EXAMPLE 
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { }
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.terraformls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  cmd = { "/Users/daniel.banck/Projects/terraform-ls/terraform-ls", "serve" }
+}
