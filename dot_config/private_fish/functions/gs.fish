efd7f4b55ec108d5666563a928ce94c2deec24e0
Daniel Banck <daniel@dbanck.de>
2024-10-28 12:21:50 +0100

    Add a couple of git aliases

---
+++ dot_config/private_fish/functions/gs.fish
@@ -0,0 +1,4 @@
+function gs --wraps='git st' --description 'alias gs=git st'
+  git st $argv
+        
+end
---
+++ dot_config/private_fish/functions/gst.fish
@@ -0,0 +1,4 @@
+function gst --wraps='git staash' --description 'alias gst=git staash'
+  git staash $argv
+        
+end
